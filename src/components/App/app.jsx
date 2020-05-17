d369c9ec932b70b2344af6d5f5f8a1b04b386473
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-05-17 15:19:56 +0200

    add hook to add note card and activate delete button

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./../Header/Header";
 import CreateNote from "./../CreateNote/CreateNote";
 import Note from "./../Note/Note";
 import Footer from "./../Footer/Footer";
 
 export default function App() {
-  function postNote(fullNote) { // receive data that send from CreateNote component
+  const [notes, setNotes] = useState([]);
+  function postNote(fullNote) {
+    // receive data that send from CreateNote component
     console.log(fullNote);
+    setNotes([...notes, fullNote]);
+  }
+  function deleteNote(id) {
+    setNotes(previous => {
+      return previous.filter((note, index) => {
+        return index !== id;
+      });
+    });
   }
   return (
     <React.Fragment>
       <Header />
       <CreateNote postNote={postNote} />
-      <Note />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            noteTitle={note.title}
+            noteContent={note.content}
+            deleteNote={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </React.Fragment>
   );
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import "./note.css";
 
-export default function Note() {
+export default function Note(props) {
+  function deleteNote() {
+    props.deleteNote(props.id);
+  }
   return (
     <div class="note-container">
-      <h4 className="note-title">Title</h4>
-      <p className="note-content">
-        bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-        bla bla bla bla bla bla bla sl;akjfda;dkjfa;sldkjfas;ldkjfas;ldkjfasl;dkjfas;dlkjfas;dlkj
-      </p>
-      <button className="delete-note">
+      <h4 className="note-title">{props.noteTitle}</h4>
+      <p className="note-content">{props.noteContent}</p>
+      <button className="delete-note" onClick={deleteNote}>
         <DeleteIcon />
       </button>
     </div>
