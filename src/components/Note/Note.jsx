0ba0d038c24e24b6af826ca7d7eff1fec0b2a9d3
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-05-17 14:20:46 +0200

    fix bug that makes text move out of the note card

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -6,7 +6,10 @@ export default function Note() {
   return (
     <div class="note-container">
       <h4 className="note-title">Title</h4>
-      <p className="note-content">bla bla bla bla bla</p>
+      <p className="note-content">
+        bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+        bla bla bla bla bla bla bla sl;akjfda;dkjfa;sldkjfas;ldkjfas;ldkjfasl;dkjfas;dlkjfas;dlkj
+      </p>
       <button className="delete-note">
         <DeleteIcon />
       </button>
