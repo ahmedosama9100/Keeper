79973d008189e87388b8a7f6e079af67e529baeb
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-05-17 14:52:39 +0200

    add animation to creating notes

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 import "./createNote.css";
 
 export default function CreateNote() {
+  const [isExpanded, setExpand] = useState(false);
+
+  document.addEventListener("click", event => {
+    const clickedElement = event.path[1].className;
+    setExpand(clickedElement === "create-note" ? true : false);
+  });
+
   return (
     <form className="create-note">
-      <input
-        name="title"
-        className="note-title"
-        placeholder="Title"
-        autoComplete="off"
-      />
+      {isExpanded && (
+        <input
+          name="title"
+          className="note-title"
+          placeholder="Title"
+          autoComplete="off"
+        />
+      )}
+
       <textarea
         name="content"
         className="note-content"
         placeholder="Take a note..."
+        row={isExpanded ? 1 : 3}
       ></textarea>
-      <button className="add-note">
-        <AddIcon />
-      </button>
+      <Zoom in={isExpanded}>
+        <Fab className="add-note">
+          <AddIcon />
+        </Fab>
+      </Zoom>
     </form>
   );
 }
